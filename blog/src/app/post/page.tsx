c5e816995b4a0cb33797c8001e866ea37e712137
Maksym <maksym0.yuzva1@gmail.com>
2023-10-29 14:21:29 +0200

    implementing custom component to check if user is authorized

--- blog/src/app/post/page.tsx
+++ blog/src/app/post/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import styles from "@/app/components/post/post.module.scss";
 import { useContext, useState } from "react";
 import { AuthContext } from "@/app/context/authContext";
 import { useForm } from "@/app/utils/hooks";
 import { useMutation } from "@apollo/client";
 import { GraphQLError } from "graphql";
+import { useRouter } from "next/navigation";
+import useCheckAuth from "../utils/useCheckAuth";
 const Post = () => {
-  const { user } = useContext(AuthContext);
+  const redirected = useCheckAuth();
+  if (redirected) {
+    return null;
+  }
+
   return <h2>Post</h2>;
 };
 
---
+++ blog/src/app/utils/useCheckAuth.tsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { useRouter } from "next/router"; // Import your router library here
+import { AuthContext } from "@/app/context/authContext";
+
+const useCheckAuth = () => {
+  const router = useRouter();
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
+    // Redirect to the "/auth" route
+    router.push("/auth");
+    return true; // Indicate that redirection occurred
+  }
+
+  return false; // Indicate that no redirection occurred
+};
+
+export default useCheckAuth;
